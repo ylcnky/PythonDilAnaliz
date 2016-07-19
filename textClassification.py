4c42dda511b3fec0f6bd47a84c0d9e00a3ca06c3
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-20 01:11:15 +0300

    words as features for learning

--- textClassification.py
+++ textClassification.py
@@ -17,6 +17,16 @@ for w in movie_reviews.words():
     all_words.append(w.lower())
 
 all_words = nltk.FreqDist(all_words)
-print(all_words.most_common(15))
 
-print(all_words['stupid'])
+word_features = list(all_words.keys())[:3000]
+
+def find_features(document):
+    words = set(document)
+    features = []
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
