0888ee03ba7f71cae81bf26f36c2663bb87e8fe7
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-19 23:25:42 +0300

    stemming added

---
+++ NLPprocessing.py
@@ -0,0 +1,15 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+
+# for w in example_words:
+#     print(ps.stem(w))
+
+new_text = "it is very important to be pythonly while you are pythoning. All pythoners have pythoned at least once"
+
+words = word_tokenize(new_text)
+
+for w in words:
+    print(ps.stem(w))
