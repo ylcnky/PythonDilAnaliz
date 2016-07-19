ee780aa29aa47b3d1a49fb83333ea883d7f619b3
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-20 00:11:21 +0300

    lemmatizing feature

---
+++ lemmatazing.py
@@ -0,0 +1,7 @@
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize('better', pos = 'a'))
+print(lemmatizer.lemmatize('best', pos = 'a'))
+print(lemmatizer.lemmatize('run', pos = 'n'))
